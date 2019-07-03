45e8006fb376e4afb870da78f9afd3065be04434
nileshdevhade <nileshdevhade555@gmail.com>
2019-07-03 15:33:03 +0530

    masterservice is here

---
+++ Masterservice/src/main/java/com/wcs/app/sb/MasterserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.app.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterserviceApplication.class, args);
+	}
+
+}
---
+++ Masterservice/src/main/java/com/wcs/app/sb/controller/Homecontroller.java
@@ -0,0 +1,9 @@
+package com.wcs.app.sb.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Homecontroller 
+{
+
+}
