82e5982249d25b8768c824166f730ce857881c9f
nileshdevhade <nileshdevhade555@gmail.com>
2019-07-03 15:20:55 +0530

    dhfdsfhsdj

---
+++ Joinsdemo/src/main/java/com/JoinsdemoApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinsdemoApplication.class, args);
+	}
+
+}
